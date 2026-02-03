1b20487c4a3758297e82065ffeb3586b3d0bed45
Martin Padron Rodriguez <mapadron@cbr2s5.42barcelona.com>
2026-02-03 19:28:48 +0100

    continued with parsing and finished data structs

--- src/parsing/data_structures.py
+++ src/parsing/data_structures.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from enum import Enum
-from pydantic import BaseModel, field_validator, Field
+from pydantic import BaseModel, field_validator, Field, ConfigDict
 
 
 class ZoneType(str, Enum):
@@ -25,35 +25,79 @@ class HubData(BaseModel):
         return v
 
 
-class ConectionData(BaseModel):
+class ConnectionData(BaseModel):
+    # Essential for Pydantic to allow custom __hash__ and set storage
+    # model_config = ConfigDict(frozen=True)
     hubs: tuple[str, str]
     max_drones: int = Field(ge=1, default=1)
+
+    @field_validator("hubs")
+    @classmethod
+    def validate_hubs(cls, v: tuple[str, str]) -> tuple[str, str]:
+        if v[0] == v[1]:
+            raise ValueError("Connection cannot link a hub to itself")
+        if v[0] > v[1]:
+            return (v[1], v[0])
+        return v
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ConnectionData):
+            return False
+        return self.hubs == other.hubs
+
+    def __hash__(self) -> int:
+        return hash(self.hubs)
 
 
 class FlyInData:
     def __init__(self) -> None:
-        self._n_drones = 0
+        self._nb_drones = 0
         self._hubs: dict[str, HubData] = {}
-        self._conections: list[ConectionData] = []
+        self._hubs_coords: set[tuple[int, int]] = set()
+        self._connections: set[ConnectionData] = set()
+        self.start = ""
+        self.end = ""
 
     @property
-    def n_drones(self) -> int:
-        return self._n_drones
+    def nb_drones(self) -> int:
+        return self._nb_drones
+
+    @nb_drones.setter
+    def nb_drones(self, drones: int) -> None:
+        if drones < 1:
+            raise ValueError("Number of drones must be a positive integer.")
+        self._nb_drones = drones
 
     @property
     def hubs(self) -> dict[str, HubData]:
         return self._hubs
 
     @property
-    def conections(self) -> list[ConectionData]:
-        return self._conections
+    def hubs_coords(self) -> set[tuple[int, int]]:
+        return self._hubs_coords
 
-    @n_drones.setter
-    def drones(self, drones: int) -> None:
-        if drones < 1:
-            raise ValueError("Number of drones must be a positive integer.")
-        self._drones = drones
+    @property
+    def connections(self) -> set[ConnectionData]:
+        return self._connections
 
     def add_hub(self, new_hub: HubData) -> None:
-        if new_hub.name in self.hubs.keys():
-            raise ValueError("")
+        if new_hub.name in self.hubs:
+            raise ValueError(
+                f"Hub name {new_hub.name!r} "
+                "is already asociated to another hub"
+            )
+        if new_hub.coords in self.hubs_coords:
+            raise ValueError(
+                f"Hub coordinates {new_hub.coords} "
+                "are already asociated to another hub"
+            )
+        self._hubs[new_hub.name] = new_hub
+        self._hubs_coords.add(new_hub.coords)
+
+    def add_connection(self, new_connection: ConnectionData) -> None:
+        if new_connection in self.connections:
+            raise ValueError("Duplicate connection")
+        for hub_name in new_connection.hubs:
+            if hub_name not in self.hubs.keys():
+                raise ValueError(f"Unknown hub {hub_name!r}")
+        self._connections.add(new_connection)
--- src/parsing/read_file.py
+++ src/parsing/read_file.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any
+from src.parsing.data_structures import HubData, ConnectionData, FlyInData
+import re
 
 
 class MapError(Exception):
@@ -9,40 +11,98 @@ class MapError(Exception):
 
 class DataProcessor(ABC):
     @abstractmethod
-    def process_data(line: str) -> Any:
+    def process_data(self, line: str, data: FlyInData) -> None:
         pass
 
 
 class DroneProcessor(DataProcessor):
-    def process_data(line: str) -> int:
-        _, n_drones = line.split(":", 1)
-        return int(n_drones)
+    def process_data(self, line: str, data: FlyInData) -> None:
+        _, nb_drones = line.split(":", 1)
+        data.nb_drones = int(nb_drones)
 
+
+class HubProcessor(DataProcessor):
+    def process_data(self, line: str, data: FlyInData) -> None:
+        data: dict[str, Any] = {}
+        hub_pattern = r"hub:\s+(\S+)\s+([+-]?\d+)\s+([+-]?\d+)(?:\s+\[(.*)\])?"
+        matches = re.match(hub_pattern, line)
+        if matches is None:
+            raise ValueError(f"unrecognized pattern {line!r}")
+
+
+
+class StartHubProcessor(DataProcessor):
+    def process_data(self, line: str, data: FlyInData) -> None:
+        pass
+
+
+class EndHubProcessor(DataProcessor):
+    def process_data(self, line: str, data: FlyInData) -> None:
+        pass
+
+
+class ConnectionProcessor(DataProcessor):
+    def process_data(self, line: str, data: FlyInData) -> None:
+        pass
 
 
 class ReadFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.file: list[str] = []
+        self.data = FlyInData()
+        self.proccesors: dict[str, DataProcessor] = {
+            "hub": HubProcessor(),
+            "start_hub": StartHubProcessor(),
+            "end_hub": EndHubProcessor(),
+            "connection": ConnectionProcessor()
+        }
 
-    def read_file(self) -> None:
+    def ignore_line(line: str) -> bool:
+        if not line or line[0] == "#":
+            return True
+        return False
+
+    def find_nb_drones(self, enum_file: enumerate[str]) -> None:
         try:
-            with open(self.filename, "r") as f:
-                file = f.read()
-                for line in file.split("\n"):
-                    line = line.strip()
-                    if line and line[0] != "#":
-                        self.file.append(line.lower())
-        except OSError as e:
-            raise MapError(e.strerror, self.filename)
+            while True:
+                line_num, line = next(enum_file)
+                line = line.strip().lower()
+                if self.ignore_line(line):
+                    continue
+                if not line.startswith("nb_drones:"):
+                    raise MapError(
+                        "expected to find 'nb_drones: <nb_drones>'",
+                        f"{self.filename}, line {line_num + 1}"
+                    )
+                DroneProcessor().process_data(line, self.data, line_num)
+
+        except StopIteration:
+            raise MapError(
+                "reached end-of-file without finding 'nb_drones'",
+                f"{self.filename}, line {line_num + 1}"
+            )
+        except ValueError as e:
+            raise MapError(
+                e, f"{self.filename}, line {line_num + 1}"
+            )
 
     def parse_file(self) -> None:
         try:
             with open(self.filename, "r") as f:
-                for line_num, line in enumerate(f):
+                enum_file = enumerate(f)
+                self.find_drones(enum_file)
+                for line_num, line in enum_file:
                     line = line.strip().lower()
-                    if not line or line[0] == "#":
+                    if self.ignore_line(line):
                         continue
+                    self.proccesors[line.split(":", 1)[0]].process_data(
+                        line, self.data
+                    )
 
         except OSError as e:
             raise MapError(e.strerror, self.filename)
+        except KeyError:
+            raise MapError(
+                f"unable to process data: {line!r}",
+                f"{self.filename}, line {line_num + 1}"
+            )
