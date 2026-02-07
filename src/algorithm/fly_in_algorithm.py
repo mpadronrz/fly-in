e2d8e312d6147305e56893270c9fa4fb65800d7c
mapadron <mpadronrz@gmail.com>
2026-02-07 10:15:49 +0100

    finished dijkstra and started path joining

--- src/algorithm/fly_in_algorithm.py
+++ src/algorithm/fly_in_algorithm.py
@@ -3,6 +3,7 @@ from src.parsing.data_structures import (
     ZoneType,
 )
 from typing import Optional
+from math import ceil
 
 
 class Vertex:
@@ -25,6 +26,15 @@ class Edge:
         self.reverse_edge: Optional["Edge"] = None
 
 
+class Path:
+    def __init__(self, vertices, cost, priority, cancelations):
+        self.vertices: list[Vertex] = vertices
+        self.cost = cost
+        self.priority = priority
+        self.nb_drones = 0
+        self.cancelations = cancelations
+
+
 class FlyInAlgorithm:
     def __init__(self) -> None:
         self.start = ""
@@ -34,6 +44,7 @@ class FlyInAlgorithm:
         self.edges: dict[tuple[str, str], Edge] = dict()
         self.cost = 0
         self.nb_drones = 0
+        self.paths: list[Path] = []
 
     def load_data(self, data: FlyInData) -> None:
         self.nb_drones = data.nb_drones
@@ -75,13 +86,77 @@ class FlyInAlgorithm:
             self.edges[(hub1, hub2)] = edge1
             self.edges[(hub2, hub1)] = edge2
 
-    def new_path_to_target(self) -> None:
+    def is_connectable(self, source, target) -> bool:
+        if self.edges[(source, target)].flow == self.edges[(source, target)].max_flow:
+            return False
+        if self.vertices[target].flow < self.vertices[target].max_flow:
+            return True
+        return self.edges[(target, source)].flow > 0
+
+    def new_path_to_target(self) -> Optional[Path]:
         parent: dict[str, str] = dict()
-        queue = [self.start]
-        distances = {vertex: 0 for vertex in self.vertices}
-        step = 1
-        while len(queue) > 0:
-            current = queue.pop()
-            cost = distances[current]
-            for vertex in self.adjacency[current]:
-                pass
+        stack = [vertex for vertex in self.vertices]
+        distances: dict[str, tuple[int | float, int]] = {vertex: (float('inf'), 0) for vertex in self.vertices}
+        distances[self.start] = (0, 0)
+
+        while len(stack) > 0:
+            current = min(stack, key=lambda x: distances[x])
+            stack.remove(current)
+            if current == self.end:
+                break
+            current_dist, current_priority = distances[current]
+            for neighbour in self.adjacency[current]:
+                edge_length = self.edges[(current, neighbour)].length
+                if distances[neighbour] > current_dist + edge_length and self.is_connectable(current, neighbour):
+                    parent[neighbour] = current
+                    if self.vertices[neighbour].priority:
+                        current_priority -= 1
+                    distances[neighbour] = (current_dist + edge_length, current_priority)
+
+        if self.end in stack:
+            return
+
+        vertices = [self.end]
+        current_vertex = self.end
+        cancelations = 0
+        while current_vertex != self.start:
+            previous_vertex = parent[current_vertex]
+            self.vertices[previous_vertex].flow += 1
+            self.edges[(previous_vertex, current_vertex)].flow += 1
+            if self.edges[(current_vertex, previous_vertex)].flow > 0:
+                cancelations += 1
+            vertices.insert(0, previous_vertex)
+        return Path(vertices, distances[self.end][0], -distances[self.end][1], cancelations)
+
+    def calculate_cost(self) -> int:
+        if not self.paths:
+            return 0
+        return (
+            ceil(
+                (sum(path.cost for path in self.paths) + self.nb_drones)
+                / len(self.paths)
+            )
+            + 1
+        )
+
+    def undo_cancelations(self, new_path: Path) -> None:
+        while new_path.cancelations > 0:
+            pass
+
+    def get_candidate_paths(self):
+        if (new_path := self.new_path_to_target()) is None:
+            return
+        self.paths.append(new_path)
+        self.cost = new_path.cost + self.nb_drones - 1
+        while len(self.paths) < self.nb_drones:
+            if (new_path := self.new_path_to_target()) is None:
+                return
+            if new_path.cost - 2 * new_path.cancelations > self.cost:
+                return
+            if new_path.cancelations > 0:
+                self.undo_cancelations(new_path)
+            self.paths.append(new_path)
+            self.cost = self.calculate_cost()
+
+    def route_optimization(self):
+        pass
